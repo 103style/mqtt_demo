607e07f157e72c9887f90bf90e7daa0d465ec58e
luoxiaoke <wtimexiaoke@gmail.com>
2019-11-01 17:04:47 +0800

    Update mqtt_topic.dart

--- mqtt_topic.dart
+++ mqtt_topic.dart
@@ -1,6 +1,6 @@
 class MQTTTopic {
   static String device_id = "";
   static String device_uid = "";
-  static String request_token = 'account/token_request/$device_id';
+  static String response_token = 'account/token_response/$device_id';
   static String request_im_bind_confirm = 'im/$device_uid/confirm';
 }
