f4b9ef2e04a998bf3241c1a943a5fa82146e6c8f
luoxiaoke <wtimexiaoke@gmail.com>
2019-11-05 17:23:52 +0800

    Update mqtt_cert.dart

--- mqtt_cert.dart
+++ mqtt_cert.dart
@@ -1,6 +1,20 @@
-///对应 ca.crt 证书的内容
-String mqtt_cert  = '''
+/// ca.crt 根 安全证书
+String cert_ca = '''
 -----BEGIN CERTIFICATE-----
 ...
 -----END CERTIFICATE-----
+''';
+
+///客户端证书  client.crt
+String cert_client_crt = '''
+-----BEGIN CERTIFICATE-----
+...
+-----END CERTIFICATE-----
+''';
+
+///client.key 客户端私钥
+String cert_client_key = '''
+-----BEGIN RSA PRIVATE KEY-----
+...
+-----END RSA PRIVATE KEY-----
 ''';
