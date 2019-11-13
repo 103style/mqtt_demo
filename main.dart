3e68e881254fa814e31b9192fd3e90ae81963864
luoxiaoke <wtimexiaoke@gmail.com>
2019-11-13 11:46:09 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,11 +6,6 @@ void main() async {
   //初始化mqtt
   int res = await MqttUtils.getInstance().init();
   print("mqtt init res = $res");
-  if (res != 0) {
-    //初始化失败 尝试先删除本地证书文件 再重新初始化
-    res = await MqttUtils.getInstance().init(deleteExist: true);
-    print("mqtt init retry res = $res");
-  }
   if (res == 0) {
    await MqttUtils.getInstance().test();
     //runApp(MyApp());
